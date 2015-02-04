c1195318d8467cea7e308f673bb0a4dde6fe6e4a
Ozan Etuzsoy <ozanetuzsoy@gmail.com>
2015-02-04 13:31:26 +0200

    Empty_form

---
+++ 150204_Win_DXBall/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Derleme hakkında Genel Bilgiler aşağıdakilerle denetlenir. 
+// kontrol edilir. Derlemeyle ilişkilendirilen bilgiyi değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("150204_Win_DXBall")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("150204_Win_DXBall")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible yanlış olarak ayarlamak bu derlemedeki türleri görünmez hale getirir 
+// COM bileşenleri için.  Bu derlemedeki bir türe erişmeniz gerekiyorsa 
+// şu türde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Eğer bu proje COM'a maruz kaldıysa aşağıdaki GUID typelib'in IDsi içindir
+[assembly: Guid("90b9f0e2-dbea-49c9-bad1-83f159c519e9")]
+
+// Bir derlemenin sürüm bilgisi bahsedilen dört değerden meydana gelir:
+//
+//      Ana Sürüm
+//      Alt Sürüm 
+//      Yapı Sayısı
+//      Düzeltme
+//
+// Bütün değerleri belirtebilir ya da Yapı ve Düzeltme Numaralarını varsayılan olarak seçebilirsiniz 
+// Yapı Numaralarını varsayılan yapabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
