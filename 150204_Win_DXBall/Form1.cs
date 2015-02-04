425ac5b364c3db48262e5b2ffefd4ad1625f9dd9
Ozan Etuzsoy <ozanetuzsoy@gmail.com>
2015-02-04 14:48:26 +0200

    sekmeler

--- 150204_Win_DXBall/Form1.cs
+++ 150204_Win_DXBall/Form1.cs
@@ -17,11 +17,24 @@ namespace _150204_Win_DXBall
             InitializeComponent();
         }
 
+        int x = 2;
+        int y = 2;
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Start();
-            misket.Left += 2;
-            misket.Top += 2;
+            misket.Left += x;
+            misket.Top += y;
+
+            if(misket.Left <= 0 | misket.Right >= this.Width)
+            {
+                x *= -1;
+            }
+
+            if(misket.Top <= 0 | misket.Bottom >= this.Height)
+            {
+                y *= -1;
+            }
         }
     }
 }
